bba1a39f5dc073458d88ecace35beb2eb10abc68
Zac <zack.nguyen@hotmail.com>
2020-08-06 20:40:49 -0400

    old and new reddit support

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,16 +1,73 @@
-import React, {useEffect} from 'react';
-import {CircularProgress, Grid} from "@material-ui/core";
+import React, {useEffect, useState} from 'react';
+import {Button, CircularProgress, Grid} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+  oldReddit: {
+    color: "#FF5700",
+    border: "1px solid #FF5700",
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    "&:hover": {
+      color: "#fff",
+      backgroundColor: "#FF5700",
+    },
+    padding: "3px 16px",
+    fontWeight: 700,
+    fontSize: 12,
+    width: 120,
+    height: 35,
+  },
+  loader:{
+    color: "#FF5700"
+  },
+  reddit: {
+    height: 35,
+    width: 120,
+    marginLeft: "2%",
+    color: "#fff",
+    border: "1px solid #FF5700",
+    backgroundColor: "#FF5700",
+    borderRadius: 4,
+    "&:hover": {
+      color: "#FF5700",
+      backgroundColor: "#fff",
+    },
+    fontWeight: 700,
+    fontSize: 12,
+    padding: "3px 16px"
+  },
+});
 
 export default function Home(): JSX.Element {
 
+  const classes = useStyles();
+
+  const [oldReddit, setOldReddit] = useState<null|boolean>(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    window.location = "https://www.reddit.com";
-  },[])
+    if(oldReddit !== null){
+      setLoading(true);
+      if(oldReddit) {
+        window.location = "https://old.reddit.com";
+      } else {
+        window.location = "https://www.reddit.com";
+      }
+    }
+  },[oldReddit])
 
   return (
     <Grid id={"loader"} container justify={"center"} alignItems={"center"}>
-      <CircularProgress  color="secondary" />
+      {loading ?
+        <CircularProgress className={classes.loader} color={"secondary"}/>
+        :
+        <React.Fragment>
+          <Button onClick={() => setOldReddit(true)} className={classes.oldReddit}> Old Reddit </Button>
+          <Button onClick={() => setOldReddit(false)} className={classes.reddit}> Reddit </Button>
+        </React.Fragment>
+      }
     </Grid>
   );
 }
