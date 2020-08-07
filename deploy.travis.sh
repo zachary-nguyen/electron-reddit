3104082da39e3fe0a1ff0640e0b30c1f937ac2b8
Zac <zack.nguyen@hotmail.com>
2020-08-07 18:41:21 -0400

    new version of yml again

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     # deploy on mac
-    yarn package
+    yarn release
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn package"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage --win"
 fi
