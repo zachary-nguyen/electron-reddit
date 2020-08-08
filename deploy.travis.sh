dbc53327ba61b09ee0fc81defbf2cb668daf3a55
Zac <zack.nguyen@hotmail.com>
2020-08-07 22:01:32 -0400

    Release 3 platforms

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn package-mac
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn package-win"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn package-win && yarn package-linux"
 fi
